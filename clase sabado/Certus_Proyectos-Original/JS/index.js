0cd4c8b30d64f8210403102f8e258b8ba40b0cd4
Laboratorio 11 Sede AQP <Arequipa-lab11@certus.edu.pe>
2024-04-22 10:50:03 -0500

    clase lunes 22 abril

---
+++ clase sabado/Certus_Proyectos-Original/JS/index.js
@@ -0,0 +1,70 @@
+/* mi primer codigo en java script*/
+window.alert("bienvenido a la pagina");
+
+
+var number_1 = 10;
+var number_2 = 12;
+var result  =  0;
+
+/*operadores aritmeticos y expresiones*/
+
+result =number_1 + number_2;
+console.log(result);
+
+result =number_1 - number_2;
+console.log(result);
+
+result =number_1 / number_2;
+console.log(result);
+
+result =number_1 * number_2;
+console.log(result);
+
+
+
+
+document.getElementById("resultado").innerHTML=" el resultado es 120";
+document.write("<h1> Certus Oficial </h1>");
+
+/*
+string frase ="mi nombre";
+int= 1  |
+float num= 2 1.1;
+Char ="A";
+bool= true false;
+*/
+
+var texto =" ";
+
+text = "inscripciones abiertas";
+document.getElementById("mensaje").innerHTML=text;
+
+var cond1 = true;
+var cond2 = false; 
+
+/*Arrays y Arreglos*/
+var nombres =["juan","maria","pedro"];
+var edades =[19,22,21];
+var datos = nombres[0]+ " "+edades[0];
+var datos = nombres[1]+ " "+edades[1];
+var datos = nombres[2]+ " "+edades[2];
+
+console.log(datos1);
+console.log(datos2);
+console.log(datos3);
+
+function sumar(value1,value2){
+    var result1 = value1+value2;
+    document.getElementById("function").innerHTML="el resultado de la suma" + sumar
+ }
+
+function sumar2(value1,value2){
+    var result1 = value1+value2;
+    return result1;
+}
+document.getElementsByName("function").innerHTML="el resultado de la suma"
+
+sumar(1,2);
+
+var nombre = getElementById("my name");
+console.log(nombre);
